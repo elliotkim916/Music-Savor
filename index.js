9f37de4ac4fdc40b397663b240cee1632dbab1c0
Elliot Kim <elliotkim916@gmail.com>
2018-01-15 12:19:07 -0800

    final adjustments

--- index.js
+++ index.js
@@ -128,29 +128,6 @@ function generateNoResults() {
     `;
 }
 
-function showInitialSearchData() {
-  $('.no-search-results').addClass('hidden');
-
-  $('.initial-search-results').on('click', '.show-initial-data-btn', function(event) {
-    event.preventDefault();
-    $('.js-initial-data').addClass('active');
-    $('.show-initial-data-btn').addClass('hidden');
-    $('.tease-read').removeClass('hide-overflow');
-    $('.read-link').removeClass('hidden');
-    $('.performance-info').prop('hidden', false);
-  });
-    
-  $('.initial-search-results').on('click','.hide-initial-data-btn', function(event) {
-    $('.js-initial-data').removeClass('active');
-    $('.show-initial-data-btn').removeClass('hidden');
-    $('.hide-initial-data-btn').addClass('hidden');
-    setTimeout(function(){
-      $('.tease-read').addClass('hide-overflow');
-      $('.performance-info').prop('hidden', true);
-    }, 800);  
-  });
-} 
-
 function generateInitalSearchResults(tDiveData, name, venueTitle, address, day, purchaseLink) {
   const {musicianName, type, teaser, readMore, youtubeUrl, youtubeID} = tDiveData;
   return `
@@ -188,12 +165,28 @@ function generateInitalSearchResults(tDiveData, name, venueTitle, address, day, 
   `;
 }
 
-function showRelatedSearchData() {
-  $('.related-search-results').on('click', '.related-artist-name', function(event) {
-    initiateRequests(event.target.text.toLowerCase());
-    $('.initial-iframe').remove();
+function showInitialSearchData() {
+  $('.no-search-results').addClass('hidden');
+
+  $('.initial-search-results').on('click', '.show-initial-data-btn', function(event) {
+    event.preventDefault();
+    $('.js-initial-data').addClass('active');
+    $('.show-initial-data-btn').addClass('hidden');
+    $('.tease-read').removeClass('hide-overflow');
+    $('.read-link').removeClass('hidden');
+    $('.performance-info').prop('hidden', false);
   });
-}
+    
+  $('.initial-search-results').on('click', '.hide-initial-data-btn', function(event) {
+    $('.js-initial-data').removeClass('active');
+    $('.show-initial-data-btn').removeClass('hidden');
+    $('.hide-initial-data-btn').addClass('hidden');
+    setTimeout(function(){
+      $('.tease-read').addClass('hide-overflow');
+      $('.performance-info').prop('hidden', true);
+    }, 800);  
+  });
+} 
 
 function generateRelatedSearchResults(data) {
   const {name, type, teaser, readMore, youtubeUrl, youtubeID} = data;
@@ -202,4 +195,11 @@ function generateRelatedSearchResults(data) {
       <a href="#" class="related-artist-name hover">${name}</a>
     </div>
   `;
+}
+
+function showRelatedSearchData() {
+  $('.related-search-results').on('click', '.related-artist-name', function(event) {
+    initiateRequests(event.target.text.toLowerCase());
+    $('.initial-iframe').remove();
+  });
 }
